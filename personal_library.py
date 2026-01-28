f10c59fb806d35f17a1a039b7bad3b4be963f801
JakeSchultz28 <jds6348@psu.edu>
2026-01-28 15:23:38 -0500

    Add main method and show menu

---
+++ personal_library.py
@@ -0,0 +1,42 @@
+"""
+Simple Personal Library
+Author: Jacob Schultz
+Version: v1
+"""
+
+def show_menu():
+    """
+    Display the main menu options for the user
+    """
+
+    print('\nPersonal Library Menu:\n'
+          '----------------------\n'
+          '1. Add a Book\n'
+          '2. Remove a Book\n'
+          '3. List All Books\n'
+          '4. Search Books\n'
+          '5. Exit\n')
+
+
+def add_book():
+    pass
+
+
+def remove_book():
+    pass
+
+
+def list_books():
+    pass
+
+
+def search_books():
+    pass
+
+
+def main():
+    show_menu()
+
+
+if __name__ == "__main__":
+    main()
